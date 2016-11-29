7bfc435eb4c44ef735af4ec3d84e6035832e8727
zhuyali <495162889@qq.com>
2016-11-29 23:35:15 +0800

    Initial Commit

---
+++ feedit_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  feedit_ios
+//
+//  Created by Julia on 16/11/29.
+//  Copyright (c) 2016年 Julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
