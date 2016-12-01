fb3b915a4a56d6e642cc72fcf9c7fe9eb2ad5836
zhuyali <495162889@qq.com>
2016-12-01 22:03:49 +0800

    first commit

--- feedit_ios/ViewController.swift
+++ feedit_ios/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://10.1.2.124:8080/feedit");
+        
+        let request = NSURLRequest(URL: url!);
+        
+        webView.loadRequest(request);
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      
     }
-
 
 }
 
